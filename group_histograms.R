2bf7bfb3110628c5aed259222e5882912d5243b4
Michael Fairley <mfai035@aucklanduni.ac.nz>
2014-11-09 19:58:07 +1300

    fixed x label

--- group_histograms.R
+++ group_histograms.R
@@ -3,6 +3,8 @@ library(lattice)
 gpa_bins=c(0,1,2,3,4,5,6,7,8,9)
 ylabp="Percent of Group"
 xlabp="GPA"
+ylimp=c(0,50)
+
 
 # 2013 Group Labels
 labels_2013=c("Group 1","Group 2","Group 3","Group 4","Group 5",
@@ -14,17 +16,17 @@ labels_2013=c("Group 1","Group 2","Group 3","Group 4","Group 5",
 # 2013 Manual Allocation
 manv.df <- read.csv("2013_manual_allocation_r.csv",header=TRUE)
 histogram( ~ Cumulative.GPA | factor(Allocated.Group, labels=labels_2013),
-           data=manv.df, breaks=gpa_bins, ylab=ylabp,GPA=xlabp,
+           data=manv.df, breaks=gpa_bins, ylab=ylabp,xlab=xlabp, ylim=ylimp,
            main="Manual Allocation GPA Histogram for Groups in 2013")
 
 # 2013 Group-Allocator
 autov.df <- read.csv("2013_group_allocator_r.csv",header=TRUE)
 histogram( ~ Cumulative.GPA | factor(Allocated.Group, labels=labels_2013),
-           data=autov.df, breaks=gpa_bins, ylab=ylabp,GPA=xlabp,
+           data=autov.df, breaks=gpa_bins, ylab=ylabp,xlab=xlabp, ylim=ylimp,
            main="Automatic Allocation GPA Histogram for Groups using 2013 Data")
 
 # 2013 Entire Class
-histogram( ~ Cumulative.GPA, data=manv.df, breaks=gpa_bins, ylab=ylabp,GPA=xlabp, main="GPA Histogram for 2013 Class")
+histogram( ~ Cumulative.GPA, data=manv.df, breaks=gpa_bins, ylab=ylabp,xlab=xlabp, ylim=ylimp, main="GPA Histogram for 2013 Class")
 
 # 2014 Group Labels
 labels_2014=c("Group 1","Group 2","Group 3","Group 4","Group 5",
@@ -36,8 +38,8 @@ labels_2014=c("Group 1","Group 2","Group 3","Group 4","Group 5",
 # 2014 Group-Allocator
 auto14.df <- read.csv("2014_group_allocator_r.csv",header=TRUE)
 histogram( ~ Cumulative.GPA | factor(Allocated.Group, labels=labels_2014),
-           data=auto14.df, breaks=gpa_bins, ylab=ylabp,GPA=xlabp,
+           data=auto14.df, breaks=gpa_bins, ylab=ylabp,xlab=xlabp, ylim=ylimp,
            main="Automatic Allocation GPA Histogram for Groups in 2014")
 
 # 2014 Entire Class
-histogram( ~ Cumulative.GPA, data=auto14.df, breaks=gpa_bins, ylab=ylabp,GPA=xlabp, main="GPA Histogram for 2014 Class")
+histogram( ~ Cumulative.GPA, data=auto14.df, breaks=gpa_bins, ylab=ylabp,xlab=xlabp, ylim=ylimp, main="GPA Histogram for 2014 Class")
